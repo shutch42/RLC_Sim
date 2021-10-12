dbaf063938a5c428a6c868f02e947eb4a96fd186
Samuel Hutcherson <shutche@siue.edu>
2021-10-11 23:52:18 -0500

    Working AC analysis

--- RLC_Sim/RLC_Sim.py
+++ RLC_Sim/RLC_Sim.py
@@ -4,13 +4,19 @@ goldenratio = .5*(1+sqrt(5))
 fsx = 7
 fsy = fsx/goldenratio
 
-def rlc(voltage = 100, inductance = .05, resistance = 20, capacitance = .0001, charge = 0, current = 0, dt = .0001, simLength = .05):
+def rlc(voltage = 120, frequency = pi*120, inductance = .2, resistance = 50, capacitance = .00001, charge = 10e-6, current = 0, dt = .0001, simLength = .25):
     numIterations = int(simLength/dt)
-    change_in_current = (voltage - resistance * current - charge / capacitance)/inductance
+    #if(inductance == 0):
+    #    current = (voltage + charge/capacitance)/resistance
+    #    change_in_current = 0;
+    #else:
+    #change_in_charge = (voltage - charge/capacitance - inductance*change_in_current)/resistance
     t = 0
     timeLst = [t]
     chargeLst = [charge]
     currentLst = [current]
+    E = voltage*cos(frequency*t)
+    change_in_current = (E - resistance*current - charge/capacitance)/inductance
 
     for i in range(1,numIterations):
         t = i * dt
@@ -19,7 +25,12 @@ def rlc(voltage = 100, inductance = .05, resistance = 20, capacitance = .0001, c
         currentLst.append(current)
         charge = charge + current*dt
         chargeLst.append(charge)
-        change_in_current = (voltage - resistance * current - charge / capacitance)/inductance
+        #if(inductance == 0):
+        #current = (voltage + charge/capacitance)/resistance
+        #else:
+        E = voltage*cos(frequency*t)
+        change_in_current = (E - resistance*current - charge/capacitance)/inductance
+        #change_in_charge = (voltage - charge/capacitance - inductance*change_in_current)/resistance
     return chargeLst, currentLst, timeLst
 
 qLst, iLst, tLst = rlc()
@@ -28,12 +39,21 @@ figure(figsize = (fsx, fsy))
 plot(tLst, qLst, 'k-')
 xlabel('Time')
 ylabel('Charge')
-title('Charge vs. Time')
+title('Charge vs. Time (DC Source)')
 show()
 
 figure(figsize=(fsx, fsy))
 plot(tLst, iLst, 'k-')
 xlabel('Time')
 ylabel('Current')
-title('Current vs. Time')
+title('Current vs. Time (DC Source)')
 show()
+
+figure(figsize=(fsx, fsy))
+plot(tLst, iLst*50, 'k-')
+xlabel('Time')
+ylabel('Current')
+title('Current vs. Time (DC Source)')
+show()
+
+print("All done!")
